e273e8f2b86315d09b3f65dcfdf558dffa3124f9
Kevin <tkevinrocket@gmail.com>
2023-05-26 20:16:52 +0700

    add pages placeholder

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path='*' element={<Navigate to='/home' replace />}/>
           <Route path='/home' element={<Homepage />} />
+          
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/components/layout/sidebar/SidebarChildren.js
+++ src/components/layout/sidebar/SidebarChildren.js
@@ -1,8 +1,17 @@
 import { Flex, Heading, Text } from "@chakra-ui/react";
-export default function SidebarChildren({ sidebarData, index }){
+import { useNavigate } from "react-router-dom";
+export default function SidebarChildren({ sidebarData, index }) {
+  const navigate = useNavigate();
 
   return (
-    <Flex border="1px solid" flexDir="column" alignItems="center" w="60">
+    <Flex
+      border="1px solid"
+      flexDir="column"
+      alignItems="center"
+      w="60"
+      _hover={{ cursor: "pointer" }}
+      onClick={() => navigate(sidebarData.url)}
+    >
       <Heading>{index}</Heading>
       <Text>{sidebarData.name}</Text>
     </Flex>
